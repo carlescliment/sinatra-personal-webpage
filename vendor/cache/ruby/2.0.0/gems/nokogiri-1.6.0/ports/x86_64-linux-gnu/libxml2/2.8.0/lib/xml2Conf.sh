45d72512fabab860f425e6febf9bc048e5e0b5c6
Carles Climent <carlescliment@hotmail.com>
2013-10-08 17:57:04 +0200

    Remove vendor dir

--- vendor/cache/ruby/2.0.0/gems/nokogiri-1.6.0/ports/x86_64-linux-gnu/libxml2/2.8.0/lib/xml2Conf.sh
+++
@@ -1,8 +0,0 @@
-#
-# Configuration file for using the XML library in GNOME applications
-#
-XML2_LIBDIR="-L/var/www/vhosts/sinatra-personal-webpage/vendor/cache/ruby/2.0.0/gems/nokogiri-1.6.0/ports/x86_64-linux-gnu/libxml2/2.8.0/lib"
-XML2_LIBS="-lxml2 -lz   -lm "
-XML2_INCLUDEDIR="-I/var/www/vhosts/sinatra-personal-webpage/vendor/cache/ruby/2.0.0/gems/nokogiri-1.6.0/ports/x86_64-linux-gnu/libxml2/2.8.0/include/libxml2"
-MODULE_VERSION="xml2-2.8.0"
-
--- vendor/cache/ruby/2.0.0/gems/thin-1.5.1/ext/thin_parser/siteconf20131008-26730-1o6ihhz.rb
+++
@@ -1,6 +0,0 @@
-require 'rbconfig'
-dest_path = "./.gem.20131008-26730-8db7ho"
-RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
-RbConfig::CONFIG['sitearchdir'] = dest_path
-RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
-RbConfig::CONFIG['sitelibdir'] = dest_path
