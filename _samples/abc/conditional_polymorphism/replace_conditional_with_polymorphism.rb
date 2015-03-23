0b3c3671a1276593bd595046cd8b0c88a5acac12
Carles Climent <carlescliment@hotmail.com>
2015-03-23 14:22:03 +0100

    Replace conditional with polymorphism, post

--- _samples/abc/conditional_polymorphism/replace_conditional_with_polymorphism.rb
+++ _samples/abc/conditional_polymorphism/replace_conditional_with_polymorphism.rb
@@ -1,11 +1,55 @@
 require 'ostruct'
 
+class ChildrensPrice
+  def price_code
+    Movie::CHILDREN
+  end
+
+  def get_charge(days_rented)
+    result = 1.5
+    result += ((days_rented-3) * 1.5) if (days_rented > 3)
+
+    result
+  end
+end
+
+class NewReleasePrice
+  def price_code
+    Movie::NEW_RELEASE
+  end
+
+  def get_charge(days_rented)
+    days_rented * 3
+  end
+end
+
+class RegularPrice
+  def price_code
+    Movie::REGULAR
+  end
+
+  def get_charge(days_rented)
+    result = 2
+    result += ((days_rented-2) * 1.5) if (days_rented > 2)
+    
+    result
+  end
+end
+
 class Rental
   attr_reader :movie, :days_rented
 
   def initialize(movie, days_rented)
     @movie = movie
     @days_rented = days_rented
+  end
+
+  def get_charge()
+    @movie.get_charge(@days_rented)
+  end
+
+  def get_frequent_renter_points()
+    @movie.get_frequent_renter_points(@days_rented)
   end
 end
 
@@ -18,7 +62,32 @@ class Movie
 
   def initialize(title, price_code)
     @title = title
-    @price_code = price_code
+    set_price_code(price_code)
+  end
+
+  def get_frequent_renter_points(days_rented)
+    if @price_code.price_code == Movie::NEW_RELEASE && days_rented > 1
+      2
+    else
+      1
+    end
+  end
+
+  def get_charge(days_rented)
+    @price_code.get_charge(days_rented)
+  end
+
+  private
+
+  def set_price_code(price_code)
+    case price_code
+    when Movie::REGULAR
+      @price_code = RegularPrice.new
+    when Movie::NEW_RELEASE
+      @price_code = NewReleasePrice.new
+    when Movie::CHILDREN
+      @price_code = ChildrensPrice.new
+    end
   end
 end
 
@@ -35,37 +104,33 @@ class Customer
   end
 
   def statement
-    total_amount = 0
-    frequent_renter_points = 0
     result = "Rental record for #{@name}"
     @rentals.each do |rental|
-      this_amount = amount_for(rental)
-      frequent_renter_points+=1 
-      frequent_renter_points+=1 if rental.movie.price_code == Movie::NEW_RELEASE && rental.days_rented >1
-      result += "\t#{rental.movie.title}\t#{this_amount}"
-      total_amount += this_amount
+      result += "\t#{rental.movie.title}\t#{rental.get_charge}"
     end
 
-    result += "Amount owed is #{total_amount}.\n"
-    result += "You earned #{frequent_renter_points} frequent renter points.\n"
+    result += "Amount owed is #{get_total_charge}.\n"
+    result += "You earned #{get_total_frequent_renter_points} frequent renter points.\n"
     result
   end
 
   private
 
-  def amount_for(a_rental)
-    the_amount = 0
-    case a_rental.movie.price_code
-    when Movie::REGULAR
-      the_amount += 2
-      the_amount += ((a_rental.days_rented-2) * 1.5) if (a_rental.days_rented > 2)
-    when Movie::NEW_RELEASE
-      the_amount += a_rental.days_rented * 3
-    when Movie::CHILDREN
-      the_amount += 1.5
-      the_amount += ((a_rental.days_rented-3) * 1.5) if (a_rental.days_rented > 3)
+  def get_total_charge
+    total_charge = 0
+    @rentals.each do |rental|
+      total_charge += rental.get_charge
     end
 
-    the_amount
+    total_charge
+  end
+
+  def get_total_frequent_renter_points
+    frequent_renter_points = 0
+    @rentals.each do |rental|
+      frequent_renter_points += rental.get_frequent_renter_points
+    end
+
+    frequent_renter_points
   end
 end
