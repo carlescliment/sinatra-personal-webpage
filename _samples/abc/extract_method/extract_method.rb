3519b2db2583989714fde23584e6777598d9b2b5
Carles Climent <carlescliment@hotmail.com>
2015-03-23 01:21:18 +0100

    Extract method, post.

--- _samples/abc/extract_method/extract_method.rb
+++ _samples/abc/extract_method/extract_method.rb
@@ -1,12 +1,15 @@
 class Printer
   def print_owning(amount)
     print_banner
-
-    puts 'The name'
-    puts 'The amount '+amount.to_s
+    print_details(amount)
   end
 
   def print_banner
-    # prints the banner
+    #prints the banner
+  end
+
+  def print_details(amount)
+    puts 'The name'
+    puts 'The amount '+amount.to_s
   end
 end
